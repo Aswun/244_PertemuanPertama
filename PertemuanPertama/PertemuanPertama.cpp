d0565f1d09fa47fc13a5d1b5bef9a54ca9c2b1ee
Aswun <aswinlutfian32@gmail.com>
2024-02-27 15:56:44 +0700

    Menambahkan sisi luas

--- PertemuanPertama/PertemuanPertama.cpp
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -9,5 +9,11 @@ int main()
     cout << "Masukan Tinggi =";
     cin >> tinggi;
     luas = alas * tinggi / 2;
+    cout << "Luasnya =" << luas << endl;
+
+    float sisi;
+    cout << "Masukan Sisi=";
+    cin >> sisi;
+    luas = sisi * sisi;
     cout << "Luasnya =" << luas;
 }
