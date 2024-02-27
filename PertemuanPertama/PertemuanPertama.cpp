9dead3a66d77f9fd3c49cdbeccbebf8ba161989a
Aswun <aswinlutfian32@gmail.com>
2024-02-27 15:29:17 +0700

    Add project files.

---
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float alas, tinggi, luas;
+    cout << "Masukan Alas =";
+    cin >> alas;
+    cout << "Masukan Tinggi =";
+    cin >> tinggi;
+    luas = alas * tinggi / 2;
+    cout << "Luasnya =" << luas;
+}
